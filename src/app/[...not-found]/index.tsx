cd6345d1bcea80d4a098e1744d17717ee730e72d
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-08-20 20:29:53 +0300

    generatestatic params

--- src/app/[...not-found]/index.tsx
+++ src/app/[...not-found]/index.tsx
@@ -2,14 +2,18 @@ import Error from "@/components/inner-pages/error";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-   title: "404 error || Homy - Real Estate React Next js Template",
+  title: "404 error || Homy - Real Estate React Next js Template",
 };
-const index = () => {
-   return (
-      <Wrapper>
-         <Error />
-      </Wrapper>
-   )
-}
 
-export default index
+export async function generateStaticParams() {
+  return [{ "not-found": [] }];
+}
+const index = () => {
+  return (
+    <Wrapper>
+      <Error />
+    </Wrapper>
+  );
+};
+
+export default index;
