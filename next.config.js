bd64524c63d0510999970e29e0b08334a10dc8b4
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-08-20 16:03:38 +0300

    configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({});
+const nextConfig = withBuilderDevTools({
+    output: 'export',
+});
 
 module.exports = nextConfig;
